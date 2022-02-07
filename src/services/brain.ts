b2b00d4fd201e78da017e5ed016fd1ab6f8da346
Alex Z <>
2022-02-07 17:02:56 -0500

    sorting possible answers by weight

--- src/services/brain.ts
+++ src/services/brain.ts
@@ -26,8 +26,6 @@ export function calculatePossibleWords(words: Word[]){
     })
   })
 
-  debugger;
-
   returnList = returnList.filter(w => {
     for(let i=0; i<excludedLetters.length; i++){
       let el = excludedLetters[i];
@@ -48,12 +46,6 @@ export function calculatePossibleWords(words: Word[]){
       }
     }
 
-    _.forEach(notHereLetters, (letters, index) => {
-      letters.forEach(nhl => {
-        if(w[index] === nhl) return false;
-      })
-    })
-
     for(let i=0; i<correctLetters.length; i++){
       let cl = correctLetters[i];
       if(cl !== '.' && w[i] !== cl) return false;
@@ -62,7 +54,64 @@ export function calculatePossibleWords(words: Word[]){
     return true;
   });
 
+  // Sort by strategic benefit of words
+  let weightedPossibleWords = weighWords(returnList)
+  weightedPossibleWords = _.orderBy(weightedPossibleWords, w => w.weight, 'desc')
+  
+  returnList = weightedPossibleWords.map(wps => wps.word);
   returnList = _.take(returnList, 15);
 
   return returnList
+}
+
+type WeightedAlphabet = WeightedLetter[]
+
+type WeightedLetter = {
+  letter: string,
+  weight: number,
+}
+
+function weighAlphabet(words: string[]): WeightedAlphabet{
+  let weightedAlphabet: WeightedAlphabet = [];
+  const conjoinedWords = words.join('');
+
+  _.forEach(conjoinedWords, l => {
+    let alphabetLetter = _.find(weightedAlphabet, alphabetLetter => alphabetLetter.letter === l);
+    if (alphabetLetter === undefined){
+      weightedAlphabet = [...weightedAlphabet, {letter: l, weight: 1} as WeightedLetter];
+    }
+    else {
+      let alphabetLetter = _.find(weightedAlphabet, alphabetLetter => alphabetLetter.letter === l)
+      if (alphabetLetter) alphabetLetter.weight++;
+    }
+  })
+
+  debugger;
+  return weightedAlphabet;
+}
+
+type WeightedWord = {
+  word: string,
+  weight: number,
+}
+
+function weighWords(words: string[]): WeightedWord[]{
+  const weightedAlphabet = weighAlphabet(words);
+
+  let returnArray: WeightedWord[] = []
+
+  _.forEach(words, word => {
+    let weightedWord: WeightedWord = {word: word, weight: 0}
+
+    let uniqWord = _.uniq(word);
+
+    _.forEach(uniqWord, letter => {
+      const alphabetLetter = _.find(weightedAlphabet, alphabetLetter => alphabetLetter.letter === letter)
+      if (alphabetLetter) weightedWord.weight += alphabetLetter.weight;
+    })
+
+    returnArray = [...returnArray, weightedWord]
+  });
+
+  return returnArray;
 }
