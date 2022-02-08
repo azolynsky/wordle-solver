14107091661a295de4ece4fc5d25d4fde2ed49de
Alex Z <>
2022-02-08 15:23:41 -0500

    can let the game play itself

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,42 @@ import Words from './components/Words';
 import _ from 'lodash';
 import { getOptimalAnswers } from './services/brain';
 import { getPossibleSolutions } from './services/solutionService';
+import wordList from './services/wordList.json'
 
-export enum LetterStatus{
+export enum LetterStatus {
   'undefined',
   'correct',
   'used',
   'unused'
 }
 
-export class Letter{
+export class Letter {
   letter: string;
   status: LetterStatus;
 
-  constructor(letter: string){
+  constructor(letter: string) {
     this.letter = letter;
     this.status = LetterStatus.undefined;
   }
 }
 
-export class Word{
+export class Word {
   letters: Letter[];
 
-  constructor(word: string){
+  constructor(word: string) {
     this.letters = word.split('').map(l => new Letter(l))
   }
 
-  getDisplayString(){
+  getDisplayString() {
     return this.letters.map(l => l.letter);
   }
 }
 
 function App() {
-  const startingWords:Word[] = [new Word('arose')];
+  //const startingWords: Word[] = [new Word('arose')];
+  const startingWords: Word[] = [];
 
+  const [secretWord, setSecretWord] = useState('');
   const [words, setWords] = useState<Word[]>(startingWords);
   const [possibleAnswers, setPossibleAnswers] = useState<string[]>([]);
   const [optimalAnswers, setOptimalAnswers] = useState<string[]>([]);
@@ -48,15 +51,39 @@ function App() {
   useEffect(() => {
     setPossibleAnswers(getPossibleSolutions(words));
     setOptimalAnswers(getOptimalAnswers(words));
-  }, [words]);
-  
+
+    if (secretWord !== '' && _.every(_.last(words)?.letters, L => L.status === LetterStatus.undefined)) {
+      let newWord = { ..._.last(words) };
+
+      if (newWord.letters) {
+        newWord.letters.forEach((L, i) => {
+          if (secretWord[i] === L.letter) {
+            L.status = LetterStatus.correct;
+          } else if (secretWord.includes(L.letter)) {
+            L.status = LetterStatus.used;
+          } else {
+            L.status = LetterStatus.unused;
+          }
+        })
+
+        setWords([...words.slice(0, words.length-1), newWord as Word])
+      }
+    }
+  }, [words, secretWord]);
+
+  useEffect(() => {
+    setWords(startingWords);
+  }, [secretWord])
+
   return (
     <div className="App">
-      <Words updateWords={(newWords: Word[]) => setWords(newWords)} words={words}/>
-      <CustomEntry addWord={(word: string) => setWords([...words, new Word(word)])}/>
+      {secretWord} <br />
+      <button onClick={() => { setSecretWord(wordList[_.random(wordList.length, false)]) }}>set new secret word</button>
+      <Words updateWords={(newWords: Word[]) => setWords(newWords)} words={words} />
+      <CustomEntry addWord={(word: string) => setWords([...words, new Word(word)])} />
       {possibleAnswers.length} possible answers
       <PossibleWords onClick={(word: string) => setWords([...words, new Word(word)])} words={_.take(optimalAnswers, possibleAnswers.length <= 1 ? possibleAnswers.length : 5)} />
-      <button onClick={() => {setWords(startingWords)}}>reset</button>
+      <button onClick={() => { setWords(startingWords) }}>reset</button>
     </div>
   );
 }
